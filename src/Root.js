434577a69e4733a4eb822076745d87d7460f5795
Stanislav Zaleski <sigmaray@gmail.com>
2019-10-07 08:51:28 +0300

    Root.js

---
+++ src/Root.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const fetch = function() {
+  "https://dog.ceo/api/breeds/image/random";
+};
+
+function Root() {
+  return <div>12345</div>;
+}
+
+export default Root;
